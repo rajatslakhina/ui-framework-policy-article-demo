964a0ea15a35288ac05ff74ce3d6c9a841e2d70f
Rajat S. Lakhina <130336875+rajatslakhina@users.noreply.github.com>
2026-09-08 13:43:09 +0530

    Add DemoApp

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import UIFrameworkPolicy
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UIFrameworkPolicyDemoView()
+        }
+    }
+}
